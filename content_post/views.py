064b8f2d7c94cf389cdc64d882c78366139392c5
Enver Bisevac <enver@bisevac.com>
2014-10-03 10:57:58 +0200

    code cleaning

--- content_post/views.py
+++ content_post/views.py
@@ -1,16 +1,14 @@
-
 from django.views.generic.dates import DateMixin
 
 from category_content.views import CategoryContentListView, CategoryContentDetailView
 from content_post import get_post_model
 
+
 class PostListView(CategoryContentListView):
     model = get_post_model()
+
 
 class PostView(DateMixin, CategoryContentDetailView):
     model = get_post_model()
     date_field = 'date_modified'
     allow_future = True
-
-    def dispatch(self, request, *args, **kwargs):
-        return super(PostView, self).dispatch(request, *args, **kwargs)
