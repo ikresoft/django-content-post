1458c7fbb560b1bd7bab4c320b8ff0529fe34288
Enver Bisevac <enver@Envers-MacBook-Pro.local>
2015-01-17 20:46:37 +0100

    better use of select2

--- content_post/url_forms.py
+++ content_post/url_forms.py
@@ -1,14 +1,16 @@
-from django.forms import forms, widgets, fields, models
-from django.core import urlresolvers
+from django.forms import models
 from django.utils.encoding import force_unicode
 
-from categories.models import Category
+from content.models import Category
+from models import Post
 from url_browser import form_registry
 from url_browser.forms import BaseUrlForm
+from content.widgets import MpttTreeWidget
+from django_select2 import AutoModelSelect2Field, AutoHeavySelect2Widget
 
 
 class PostCategoryIndexForm(BaseUrlForm):
-    categories = models.ModelMultipleChoiceField(queryset=Category.objects.all())
+    categories = models.ModelMultipleChoiceField(required=True, queryset=Category.objects.all(), widget=MpttTreeWidget)
 
     def get_path(self):
         paths = []
@@ -27,3 +29,22 @@ class PostCategoryIndexForm(BaseUrlForm):
         url_name = 'post_archive_index'
 
 form_registry.register('post_archive_index', PostCategoryIndexForm)
+
+
+class PostChoicesField(AutoModelSelect2Field):
+    queryset = Post.objects
+    search_fields = ['title__icontains', "body__icontains"]
+
+
+class PostDetailForm(BaseUrlForm):
+    post = PostChoicesField(widget=AutoHeavySelect2Widget(attrs={"style": "width:400px;"}))
+
+    def submit(self):
+        url = "'%s' %d" % (self.Meta.url_name, self.cleaned_data["post"].pk)
+        return url
+
+    class Meta:
+        verbose_name = 'Choose post'
+        url_name = 'category_post_detail'
+
+form_registry.register('category_post_detail', PostDetailForm)
